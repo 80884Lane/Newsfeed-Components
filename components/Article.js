f4ca5aa9f9b2233f2cde037455fdbdf1f22a0405
Bobby Lane <bobbylane@Bobbys-MacBook-Pro.local>
2020-12-02 20:14:24 -0500

    Components Web38 Take 1

--- components/Article.js
+++ components/Article.js
@@ -92,9 +92,30 @@ const data = [
 /*
   Step 1: Write a component called 'articleMaker' to create an article.
   Your component is a function that takes an article object as its only argument,
-  and returns a DOM node looking like the one below:
+  and returns a DOM node looking like the one below:*/
+function articleMaker({artObj}) {
+  const article = document.createElement("div");
+  const title = document.createElement("h2");
+  const date = document.createElement("p");
+  const expButton = document.createElement("span");
 
-  <div class="article">
+  article.appendChild(title);
+  article.appendChild(date);
+  article.appendChild(span);
+
+  article.classList.add(".article");
+  title.classList.add("h2");
+  date.classList.add("p");
+  expButton.classList.add("span");
+
+
+
+  expButton.addEventListener("click", (event) => {
+    expButton.classList.toggle("article-open");
+  });
+  return article;
+}
+/*  <div class="article">
     <h2>{title of the article}</h2>
     <p class="date">{date of the article}</p>
 
@@ -109,8 +130,19 @@ const data = [
   Step 3: Don't forget to return something from your function!
 
   Step 4: Outside your function now, loop over the data. At each iteration you'll use your component
-  to create a div.article element and append it to the DOM inside div.articles (see index.html).
+  to create a div.article element and append it to the DOM inside div.articles (see index.html).*/
 
-  Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
+  const artElements = data.map((data) => {
+    return articleMaker(data);
+  });
+
+  artElements.forEach((artElements) => {
+    article.appendChild(artElements);
+  });
+
+
+
+/*  Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
   Refresh the page to see the new article.
 */
+articleMaker()
--- components/Menu.js
+++ components/Menu.js
@@ -10,9 +10,27 @@ let menuItems = [
 ];
 
 /* 
-  Step 1: Write a component called 'menuMaker' to create a menu like the markup below:
+  Step 1: Write a component called 'menuMaker' to create a menu like the markup below:*/
 
-  <div class="menu">
+ 
+
+
+  function menuMaker(array) {
+    
+    const menu = document.createElement("div");
+    const items = document.createElement("ul");
+    const each = document.createElement("li");
+
+    menu.appendChild(items);
+    items.appendChild(each);
+
+    menu.classList.add(".menu");
+    items.classList.add([]);
+    
+
+  }
+
+/*  <div class="menu">
     <ul>
       {each menu item as an <li>}
     </ul>
@@ -21,13 +39,38 @@ let menuItems = [
   The 'menuMaker' takes an array of menu items as its only argument.
 
   Step 2: Inside the function, iterate over the array creating a list item <li> element for each item in the array.
-  Add those items to the <ul>
+  Add those items to the <ul>*/
+  const menuElements = menu.map((menu) => {
+    return menuMaker(menu);
+  });
 
-  Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button').
+  menuElements.forEach((menuElement) => {
+    menu.appendChild(menuElements);
+  });
 
-  Step 4: Add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on div.menu (your div with a 'menu' class).
 
-  Step 5: Don't forget to return your div.menu.
 
-  Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
+  /*Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button').*/
+
+  const menuButton = document.querySelector(".menu-button");
+
+
+/*  Step 4: Add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on div.menu (your div with a 'menu' class).*/
+
+  menuButton.addEventListener("click", (event) => {
+    menu.classList.toggle("menu-open");
+  });
+
+/*  Step 5: Don't forget to return your div.menu.*/
+return menuButton;
+
+/*  Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
 */
+const newMenu(['Students',
+'Faculty',
+"What's New",
+'Tech Trends',
+'Music',
+'Log Out']
+
+menu.appendChild(newMenu);
